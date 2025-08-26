896213e7c431c85a12b36c2da23f85f5784724ca
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-08-26 18:30:36 +0200

    change

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ if (file_exists($maintenance = __DIR__ . '/../reservation-system-backend/storage
 // // Bootstrap Laravel and handle the request...
 // $app = require_once __DIR__ . '/../reservation-system-backend/bootstrap/app.php';
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
