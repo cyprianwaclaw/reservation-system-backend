97e8919fd0318ce0b71346c12b772ffb119c9702
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-08-28 16:13:04 +0200

    Add new doctor seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Doctor;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +15,38 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Doctor::create([
+            'name' => 'Michał',
+            'surname' => 'Kaczmarek',
+            'phone' => '123456789',
+            'email' => 'test@test.pl',
+            'password' => bcrypt('haslo123'),
+        ]);
+        Doctor::create([
+            'name' => 'Grzegorz',
+            'surname' => 'Roczniak',
+            'phone' => '123456789',
+            'email' => 'test1@test.pl',
+            'password' => bcrypt('haslo123'),
+        ]);
+        Doctor::create([
+            'name' => 'Asia',
+            'surname' => 'Jachym-Drewniak',
+            'phone' => '123456789',
+            'email' => 'test2@test.pl',
+            'password' => bcrypt('haslo123'),
+        ]);
+        Doctor::create([
+            'name' => 'Ola',
+            'surname' => 'Test',
+            'phone' => '123456789',
+            'email' => 'test3@test.pl',
+            'password' => bcrypt('haslo123'),
         ]);
     }
 }
