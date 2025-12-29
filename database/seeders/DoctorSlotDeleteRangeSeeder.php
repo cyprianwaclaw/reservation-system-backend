03a3597294257a2de7d0cbf018471a21c2e34a3a
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-12-29 23:15:04 +0100

    test

--- database/seeders/DoctorSlotDeleteRangeSeeder.php
+++ database/seeders/DoctorSlotDeleteRangeSeeder.php
@@ -10,8 +10,8 @@ class DoctorSlotDeleteRangeSeeder extends Seeder
     public function run(): void
     {
         // USTAW SWÓJ ZAKRES DAT
-        $from = '2025-12-06'; // od tej daty
-        $to   = '2026-02-28'; // do tej daty włącznie
+        $from = '2026-02-16'; // od tej daty
+        $to   = '2026-02-16'; // do tej daty włącznie
 
         DoctorSlot::whereBetween('date', [$from, $to])->delete();
     }
